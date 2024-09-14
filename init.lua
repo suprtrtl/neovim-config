61f52b95da40ea1cf41722f3ed8fdcacf4b6511e
suprtrl <owen.rodway@gmail.com>
2024-09-14 19:59:41 -0400

    basic setup

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("suprtrtl")
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Tutorial Game for Neovim
+Plug('ThePrimeagen/vim-be-good')
+
+vim.call('plug#end')
