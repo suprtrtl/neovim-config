d5067c2bd413f579f7f07fcabec145b6c4ef5398
suprtrl <owen.rodway@gmail.com>
2024-09-16 18:42:00 -0400

    Added LSP

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,9 @@
+return { 
+	'VonHeikemen/lsp-zero.nvim', 
+	branch = 'v4.x',
+	dependencies = {
+		'neovim/nvim-lspconfig',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/nvim-cmp',
+	}
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,8 @@
+return { 
+	"williamboman/mason.nvim",
+
+	dependencies = {
+    		"williamboman/mason-lspconfig.nvim",
+    		"neovim/nvim-lspconfig",
+	}
+}
