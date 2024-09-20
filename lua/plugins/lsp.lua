fbbe08ea78280f2f397ee979a03b9eb0c4361075
suprtrl <owen.rodway@gmail.com>
2024-09-20 17:45:45 -0400

    Refactored Lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,116 +1,39 @@
 return {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v4.x',
+	'neovim/nvim-lspconfig',
 	dependencies = {
-		'neovim/nvim-lspconfig',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/nvim-cmp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'L3MON4D3/LuaSnip',
 	},
 
 	config = function ()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			local lsp_zero = require('lsp-zero')
-                
-            local cmp_lsp = require("cmp_nvim_lsp")
-
-
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_lsp.default_capabilities() )
-
-
-		-- lsp_attach is where you enable features that only work
--- if there is a language server active in the file
-	local lsp_attach = function(client, bufnr)
-		local opts = {buffer = bufnr}
-
-		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-		vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
- 		vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  		vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-  		vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-  		vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-		vim.keymap.set('n', '<F1>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-		vim.keymap.set({'n', 'x'}, '<F2>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  		vim.keymap.set('n', '<F3>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-	end
-
-	
-	lsp_zero.extend_lspconfig({
-			sign_text = true,
-			lsp_attach = lsp_attach,
-			capabilities = require('cmp_nvim_lsp').default_capabilities(),
-	})
-
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"gopls",
-			},
-			handlers = {
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup {
-						capabilities = capabilities
-					}
-				end,
-
-				zls = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.zls.setup({
-						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-						settings = {
-							zls = {
-								enable_inlay_hints = true,
-								enable_snippets = true,
-								warn_style = true,
-							},
-						},
-					})
-					vim.g.zig_fmt_parse_errors = 0
-					vim.g.zig_fmt_autosave = 0
-				end,
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup {
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-								}
-							}
-						}
-					}
-				end,
-			}
-		})
+        local lsp = require('lspconfig')
+        
+        
+        local servers = {
+            'clangd',
+            'gopls',
+            'pyright',
+            'rust_analyzer',
+            'ts_ls',
+        }
 
 
-		local cmp = require('cmp')
-		local cmp_select = {behavior = cmp.SelectBehavior.Select}
+        -- Iterates through servers in lua/config/servers.lua
+        for _, s in ipairs(servers) do
+            lsp[s].setup {
 
-		cmp.setup({
-			mapping = cmp.mapping.preset.insert( {
+                -- Adds Defualt Capabilities
+                capabilities = capabilities
+            }
+        end
 
-				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-				['<Tab>'] = cmp.mapping.confirm( { select = true } ),
-				['<C-Space>'] = cmp.mapping.complete(),
-			} ),
 
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-            }, {
-                { name = 'buffer' },
-            })
-		} )
+
+
+
 	end
 }
