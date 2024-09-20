c7848d0b4fd7abcbb8b33d91614612c0e78bef01
suprtrl <owen.rodway@gmail.com>
2024-09-20 15:52:06 -0400

    Addded Lualine

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,9 +4,6 @@ function ColorMyPencils(color)
 	color = color or "ayu"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0,  "Normal", { bg = "none" })
-
-	vim.api.nvim_set_hl(0,  "NormalFloat", { bg = "none" })
 end
 
 return { {
